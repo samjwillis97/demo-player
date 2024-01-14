df245e3ef3aa8eb89235285d4a1cf83d55647924
Sam Willis <sam@williscloud.org>
2024-01-14 13:49:03 +1100

    File buffer being passed into Golang :)

--- src/demoparser/main.go
+++ src/demoparser/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/teamortix/golang-wasm/wasm"
+	"syscall/js"
 )
 
 const (
@@ -9,12 +10,31 @@ const (
 	msgQueueBufferSize = 128 * 1024
 )
 
+var (
+	fileLength = 0
+)
+
 func test() (string, error) {
 	return "Hello, from Golang!", nil
 }
 
+func getFileLength() int {
+	return fileLength
+}
+
+func passFileToGo(this js.Value, p js.Value) interface{} {
+	typedArray := js.Global().Get("Uint8Array").New(p)
+	dataArray := make([]byte, typedArray.Length())
+	js.CopyBytesToGo(dataArray, typedArray)
+	return (len(dataArray))
+}
+
 func main() {
-	wasm.Expose("test", test)
+	c := make(chan struct{}, 0)
+
+	wasm.Expose("passFileToGo", passFileToGo)
+
 	wasm.Ready()
-	<-make(chan struct{}, 0)
+
+	<-c
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
+  import { Label } from "$lib/components/ui/label";
   import { onMount } from "svelte";
+  import { twMerge } from "tailwind-merge";
   import parser from "../demoparser/main.go";
 
   onMount(async () => {
     console.log(await parser.test());
   });
+
+  let isLoading: boolean = false
+  let files: FileList
+
+  $: if (files && files.length > 0) {
+    handleFile(files[0]).then();
+  }
+
+  const handleFile = async (file: File) => {
+    if (!files || files.length === 0) return;
+    isLoading = true;
+
+    const fileBuffer = await file.arrayBuffer();
+    console.log(await parser.passFileToGo(fileBuffer));
+
+    isLoading = false;
+  }
+
 </script>
 
-<div class="flex flex-col h-full justify-center items-center" />
+<div class="flex flex-col h-full justify-center items-center">
+	<div class="flex flex-col max-w-sm gap-1.5">
+		<Label for="demo">Demo</Label>
+		<input
+			disabled={isLoading}
+			id="demo"
+			type="file"
+			class={twMerge(
+        "flex h-9 w-full rounded-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+        )}
+			bind:files
+		/>
+	</div>
+</div>
